97680ebfc977fb3a54af6f4170d9c32deb5bc719
Frère Jacques <zyb@zjex.com>
2016-11-02 11:09:33 +0800

    add actions

--- app/modules/components/users-section/component.js
+++ app/modules/components/users-section/component.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+    currentUser: null,
+    actions: {
+        selectUser(user) {
+            set(this, 'currentUser', user);
+        },
+
+        clearUser() {
+            set(this, 'currentUser', null);
+        }
+    }
+
+}).reopenClass(positionParams:["users"]);
+
