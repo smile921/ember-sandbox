d3570bb6b4cfe3b6a5ddd0d7f2d6ffe9a9730f30
Frère Jacques <zyb@zjex.com>
2016-11-02 13:07:42 +0800

    fix

--- app/modules/components/users-section/user-detail-section/component.js
+++ app/modules/components/users-section/user-detail-section/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
-
 export default Ember.Component.extend({
-});
+    tagName:'div',
+    classNames:['cell','-40f12']
+}).reopenClass({ positionalParams:['currentUser']});
