90102e8e164ed3c50c2ce1f4dafdc422443cf312
Frère Jacques <zyb@zjex.com>
2016-11-02 13:06:29 +0800

    重构

--- app/modules/components/users-section/user-list-section/component.js
+++ app/modules/components/users-section/user-list-section/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
+import styles from '../../../index/styles';
+
 export default Ember.Component.extend({
-});
+    tagName:'ul',
+    styles,
+    classNames:['cell','-4of12'],
+    localClassNames:['user-list']
+}).reopenClass({ positionalParams:['api']});
